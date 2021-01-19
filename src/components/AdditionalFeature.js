0ea34f2cc5fdf9a598ca59803d25eb449c163266
bdiaz12345 <bdiazmusic5@gmail.com>
2021-01-19 08:22:21 -0500

    mvp finally

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { addFeature } from '../actions/index'
+import { connect } from 'react-redux'
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => {
+        props.dispatch(addFeature(props.feature))
+      }} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = (state, props) => {
+  console.log(state)
+  console.log(props.feature)
+  return {
+    ...state,
+    feature: props.feature 
+  }
+}
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
